4a9226e3eb1a120dc7cff1d3d9e3a66601dfd5a2
jsemyonom <jsemyonom@gmail.com>
2014-08-14 00:53:38 +0800

    update more

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ default
 =======
 my default project
 gulp usage
+now functions:
+1.Server + livereload
+2.Less compiler
+3.JS suite + libs compiler
+4.All production files concat and minified;
--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,136 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var imagemin = require('gulp-imagemin');
-var minifyHTML = require('gulp-minify-html');
 var autoprefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
-var notify = require("gulp-notify");
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var gulpif = require('gulp-if');
-var uncss = require('gulp-uncss');
-var clean = require('gulp-clean');
-
-//сервер
-var connect = require('gulp-connect');
-//склеивает все файлы
 var concat = require('gulp-concat');
 
+
+var livereload = require('tiny-lr');
+
+//Пути
 var lessSrc = './src/less/*.less',
+    lessDst = './src/css',
+    lessSrcWatch = './src/less/**/*.less',
+    cssSrc = './src/css/**/*.css',
     cssDst = './dist/css',
-    cssSrc = './css/**/*.css',
     jsSrc = './src/js/**/*.js',
     jsDst = './dist/js',
-    htmlSourseSrc = './src/*.html',
-    htmlSrc = './*.html',
+    htmlSrc = './src/*.html',
     htmlDst = './',
     imgSrc = './src/img/**/*',
     imgDst = './dist/img';
 
-var minify = false;
+//JS билиоткеи
+var bower_scripts = [
+    './bower_components/jquery/dist/jquery.js'
+];
 
+//Стили для JS библиотек
+var libs_styles = [
+    ''
+];
 
 gulp.task('less', function () {
     gulp.src(lessSrc)
         .pipe(less())
         .pipe(autoprefix(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+        .pipe(gulp.dest(lessDst));
+});
+
+gulp.task('css_production', function () {
+    //сборка библиотек
+    gulp.src(libs_styles)
+        .pipe(concat('libs.css'))
         .pipe(gulp.dest(cssDst))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifyCSS())
+        .pipe(gulp.dest(cssDst));
+
+    //сбока всех стилей и минимизация
+    gulp.src(lessSrc)
+        .pipe(less())
+        .pipe(autoprefix(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+        .pipe(concat('style.css'))
         .pipe(gulp.dest(cssDst))
-        .pipe(uncss({
-            html: ['index.html']
-        }))
-        .pipe(rename({suffix: '.light'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(cssDst));
+});
+
+gulp.task('css_development', function () {
+    //сбока всех стилей и минимизация
+    gulp.src(cssSrc)
+        .pipe(concat('style.css'))
         .pipe(gulp.dest(cssDst))
-        .pipe(notify({ message: 'LESS task complete' }));
 });
 
 gulp.task('js', function () {
     gulp.src(jsSrc)
-        .pipe(concat('main.js'))
+        .pipe(concat('suite.js'))
         .pipe(gulp.dest(jsDst))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
+        .pipe(gulp.dest(jsDst));
+
+    gulp.src(bower_scripts)
+        .pipe(concat('libs.js'))
         .pipe(gulp.dest(jsDst))
-        .pipe(notify({ message: 'Scripts task complete' }));
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDst));
 });
 
 gulp.task('img', function () {
     gulp.src(imgSrc)
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest(imgDst))
-        .pipe(notify('Images task complete'));
+        .pipe(gulp.dest(imgDst));
 });
 
 gulp.task('html', function () {
-    gulp.src(htmlSourseSrc)
-        .pipe(gulpif(minify, minifyHTML({
-            spare: true,
-            conditionals: true,
-            cdata: true
-        })))
-        .pipe(gulp.dest(htmlDst))
-        .pipe(notify({ message: 'HTML task complete' }));
+    gulp.src(htmlSrc)
+        .pipe(gulp.dest(htmlDst));
 });
 
-gulp.task('reload', function () {
-    gulp.src('')
-        .pipe(connect.reload())
-        .pipe(notify('dist data reload'));
+gulp.task('livereload', function () {
+    livereload = livereload();
+    livereload.listen(4002);
 });
 
-//server + watch powerful
 gulp.task('server', function () {
-    connect.server({
-        livereload: true
-    });
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')({port: 4002}));
+    app.use(express.static(__dirname));
+    app.listen(4000);
 });
 
 gulp.task('watch', function () {
-    gulp.watch([lessSrc], ['less']);
-    gulp.watch([htmlSourseSrc], ['html']);
-    gulp.watch([cssSrc], ['reload']);
-    gulp.watch([htmlSrc], ['reload']);
-    gulp.watch([imgSrc], ['reload']);
+    gulp.watch([lessSrcWatch], ['less']);
+    gulp.watch([htmlSrc], ['html']);
+    gulp.watch([imgSrc], ['img']);
+    gulp.watch([cssSrc], ['css_development']);
+    gulp.watch(['./dist/**/*.*', './*.html'], notifyLiveReload);
 });
 
-gulp.task('default', ['less', 'js', 'img', 'html']);
-gulp.task('dev', ['server', 'default', 'watch']);
+gulp.task('default', ['css_production', 'js', 'img', 'html']);
+
+gulp.task('dev', ['server', 'less', 'css_development' , 'js', 'img', 'html', 'livereload', 'watch']);
+
+function notifyLiveReload(event) {
+    var fileName = require('path').relative(__dirname, event.path);
+
+    livereload.changed({
+        body: {
+            files: [fileName]
+        }
+    });
+
+    console.log("file changed: ==" + fileName + "==");
+}
+
 
 
 
