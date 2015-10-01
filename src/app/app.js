000df52eb1ebb1b49bbf5bbb5a9593551e01167a
jsemyonom <jsemyonom@gmail.com>
2015-10-01 11:52:50 +0700

    update

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,8 @@
-import "angular";
+var $ = require('jquery');
+var paper = require('paper/dist/paper-full.js');
 
+var worldElement = document.getElementById('world');
+//var start = new Point(100, 100);
 
+//paper.setup(paper);
+console.log(paper);
